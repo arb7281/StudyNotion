4d427c1d83a3dda4d468ab10cc171e5f84b573c5
arb7281 <arb7281@gmail.com>
2024-01-24 18:32:13 +0530

    cart component cahnges renderCourses and render total amount is pending

--- src/components/core/Dashboard/cart/index.js
+++ src/components/core/Dashboard/cart/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const index = () => {
+
+    const {total, totalitems} = useSelector((state) => state.cart)
   return (
-    <div>index</div>
+    <div>
+        <h1>your Cart</h1>
+        <p>{totalitems} Courses in cart</p>
+        {
+            total > 0 
+            ? (<div>
+                <RenderCartCourses/>
+                <RenderTotalAmount/>
+                </div>) 
+            : (<div>Your cart is empty</div>)
+        }
+    </div>
   )
 }
 
